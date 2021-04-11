7652b5c022aaf6c35a096e346037b2dbfd5f291e
Kenneth G KittsJr <Kenneth.G.KittsJR@aexp.com>
2021-04-11 10:24:00 -0700

    Added reverse method to LinkedList class

--- LinkedList.py
+++ LinkedList.py
@@ -23,11 +23,11 @@ class LinkedList:
 
     def print_list(self):
         if self.head is None:
-            print('None')
+            print('List is empty')
             return
         current_node = self.head
         while current_node is not None:
-            print(current_node.value, '=>')
+            print(current_node.value, '=> ', end='')
             current_node = current_node.next_node
         print('None')
         return
@@ -40,4 +40,36 @@ class LinkedList:
         node.next_node = self.head
         self.head = node
 
+    def reverse(self):
+        if not self.head:
+            print("There is no head of the list.")
+            return
+        if self.head.next_node is None:
+            print("Only one element in list")
+            return
 
+        prev_node = None
+        curr_node = self.head
+        next_node = self.head.next_node
+
+        while True:
+            curr_node.next_node = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+            if next_node is None:
+                break
+            next_node = next_node.next_node
+        self.head = prev_node
+        return
+
+
+ll = LinkedList()
+
+for i in range(0):
+    ll.append(i)
+
+ll.print_list()
+
+ll.reverse()
+
+ll.print_list()
